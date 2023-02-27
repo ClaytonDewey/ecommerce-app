6782e1e349d50b056d10cbc0f966d9f69db6306a
Clayton Dewey <clay@dryadmedia.com>
2023-02-26 21:20:07 -0600

    Update Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,17 +1,20 @@
+import { Flex } from '@chakra-ui/react';
 import React from 'react';
 
 const Navbar: React.FC = () => {
   return (
-    <nav>
-      <h1>sneakers</h1>
-      <ul>
-        <li>Collections</li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
+    <>
+      <Flex alignItems='center' justifyContent='center' height='113px'>
+        <h1>sneakers</h1>
+        <ul>
+          <li>Collections</li>
+          {/* <li>Men</li>
+            <li>Women</li>
+            <li>About</li>
+            <li>Contact</li> */}
+        </ul>
+      </Flex>
+    </>
   );
 };
 export default Navbar;
